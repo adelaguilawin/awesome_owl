8e7db2a0601385162971e3d0b2d19f9dd1c06c0d
adelaguila <adelaguila@winempresas.pe>
2024-11-08 17:22:36 -0500

    restore assets

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Owl tutorial by Jack",
+    'name': "Awesome Owl by Jack",
 
     'summary': """
         Starting module for "Discover the JS framework, chapter 1: Owl components"
@@ -26,5 +26,17 @@
     'data': [
         'views/templates.xml',
     ],
+    'assets': {
+        'awesome_owl.assets_playground': [
+            ('include', 'web._assets_helpers'),
+            'web/static/src/scss/pre_variables.scss',
+            'web/static/lib/bootstrap/scss/_variables.scss',
+            'web/static/lib/bootstrap/scss/_maps.scss',
+            ('include', 'web._assets_bootstrap'),
+            ('include', 'web._assets_core'),
+            'web/static/src/libs/fontawesome/css/font-awesome.css',
+            'awesome_owl/static/src/**/*',
+        ],
+    },
     'license': 'AGPL-3'
 }
