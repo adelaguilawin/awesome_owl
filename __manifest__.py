1314403b471e8959e779ac767f2efe82e93ad77d
adelaguila <adelaguila@winempresas.pe>
2024-11-08 17:03:41 -0500

    update playground

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Awesome Owl by Jack",
+    'name': "Owl tutorial by Jack",
 
     'summary': """
         Starting module for "Discover the JS framework, chapter 1: Owl components"
@@ -26,17 +26,5 @@
     'data': [
         'views/templates.xml',
     ],
-    'assets': {
-        'awesome_owl.assets_playground': [
-            ('include', 'web._assets_helpers'),
-            'web/static/src/scss/pre_variables.scss',
-            'web/static/lib/bootstrap/scss/_variables.scss',
-            'web/static/lib/bootstrap/scss/_maps.scss',
-            ('include', 'web._assets_bootstrap'),
-            ('include', 'web._assets_core'),
-            'web/static/src/libs/fontawesome/css/font-awesome.css',
-            'awesome_owl/static/src/**/*',
-        ],
-    },
     'license': 'AGPL-3'
 }
